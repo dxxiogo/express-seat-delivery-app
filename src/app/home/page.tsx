0b6bc9f77911df168ecd38caaa41e36c8fc9a97f
lucio souza <desouzaleitelucioweslley@gmail.com>
2025-07-14 08:43:01 -0300

    adcionando novas telas

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,42 @@
+import Sidebar from "@/components/sidebar";
+import HeaderSearch from "@/components/search-header";
+import ItemPreview from "@/components/item-preview";
+import RestaurantPreview from "@/components/restaurant-preview";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex min-h-screen">
+
+      <Sidebar />
+
+      <div className="flex-1 bg-white">
+        <HeaderSearch />
+
+        <section className="mt-8 px-8">
+          <h2 className="text-xl font-bold mb-4">Melhores avaliados</h2>
+          <div className="flex gap-4 overflow-x-auto">
+            <Link href="/item/1">
+            <ItemPreview />
+            </Link>
+            <ItemPreview />
+            <ItemPreview />
+            <ItemPreview />
+          </div>
+        </section>
+
+        <section className="mt-8 px-8">
+          <h2 className="text-xl font-bold mb-4">Melhores avaliados</h2>
+          <div className="flex gap-4 overflow-x-auto">
+            <Link href="/restaurant/1">
+            <RestaurantPreview />
+            </Link>
+            <RestaurantPreview />
+            <RestaurantPreview />
+            <RestaurantPreview />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
--- src/components/text-field.tsx
+++ src/components/text-field.tsx
@@ -1,16 +1,19 @@
+'use client';
 import React, { useState } from 'react';
 
-const PlaceholderInput = () => {
+type PlaceholderInputProps =  {
+  className?: string;
+  placeholder?: string;
+};
+
+const PlaceholderInput = ({ className = '', placeholder}: PlaceholderInputProps) => {
   const [value, setValue] = useState('');
 
   return (
-    <div className="w-full max-w-md">
+    <div className={className}>
       <input
-        type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        placeholder="Placeholder"
-        className="w-full px-4 py-3 border border-gray-300 rounded-lg text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
+      placeholder={placeholder}
+        className={`w-full px-4 py-3 border border-gray-300 rounded-lg text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200 ${className || ''}`}
       />
     </div>
   );
